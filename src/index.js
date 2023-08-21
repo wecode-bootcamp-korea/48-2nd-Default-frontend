01011cabad80fcc795549a6b1cb2a3a4462cd68a
llinjae <dldlswognqh@naver.com>
2023-08-21 16:50:36 +0900

    sass로 변경

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
-
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from './styles/GlobalStyle';
-import theme from './styles/theme';
+import './styles/reset.scss';
+import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Router />
-  </ThemeProvider>,
-);
+root.render(<Router />);
--- src/styles/theme.js
+++
@@ -1,9 +0,0 @@
-const size = {
-  tablet: '640px',
-  laptop: '1200px',
-  desktop: '1800px',
-};
-
-const theme = { lightGray: '#939292' };
-
-export default theme;
