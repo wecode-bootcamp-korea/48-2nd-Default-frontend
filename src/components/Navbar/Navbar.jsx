f82d61ac8be22df3a2cc591b1751ed048cdecec7
llinjae <dldlswognqh@naver.com>
2023-08-23 11:23:33 +0900

    Add : 각 컴포넌트 생성 및 배치

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Navbar.scss';
+import Logo from './Logo';
+import Search from './Search';
+import UserMenu from './UserMenu';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbarContainer">
+      <div className="navbarContentBox">
+        <Logo />
+        <Search />
+        <UserMenu />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Main;
