54dc4e21904689aeb04f6934e30b5d814ff898dc
llinjae <dldlswognqh@naver.com>
2023-08-26 03:00:48 +0900

    Remove : context 삭제

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ModalProvider } from './context/ModalContext';
 import DetailPage from './pages/DetailPage/DetailPage';
 import Navbar from './components/Navbar/Navbar';
 import Main from './pages/Main/Main';
