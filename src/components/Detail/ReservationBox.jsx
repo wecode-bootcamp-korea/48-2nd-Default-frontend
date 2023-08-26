636cf8ea6c81fa8836afdf7b738a5a8fc2380f94
llinjae <dldlswognqh@naver.com>
2023-08-26 12:18:23 +0900

    Add : detail Page UI 완성

--- src/components/Detail/ReservationBox.jsx
+++ src/components/Detail/ReservationBox.jsx
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AiFillStar } from 'react-icons/ai';
 import './ReservationBox.scss';
 
 const ReservationBox = () => {
-  return <div className="reservationContainer">ReservationBox</div>;
+  const [gradientPosition, setGradientPosition] = useState({ x: 0, y: 0 });
+  const navigate = useNavigate();
+
+  const handleMouseMove = event => {
+    const x = event.nativeEvent.offsetX;
+    const y = event.nativeEvent.offsetY;
+
+    setGradientPosition({ x, y });
+  };
+
+  const handleReservationSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="reservationContainer">
+      <div className="reservationHeader">
+        <p className="priceBox">
+          <b className="price">￦360,000</b> / 박
+        </p>
+        <div className="rating">
+          <AiFillStar />
+          <p>4.85</p>
+          <p className="ratingReview">후기 52개</p>
+        </div>
+      </div>
+      <div className="reservationBody">
+        <form className="checkDateBox" onSubmit={handleReservationSubmit}>
+          <button className="checkDate">
+            <div>
+              <p>체크인</p>
+              <p>2023.9.5</p>
+            </div>
+            <div>
+              <p>체크아웃</p>
+              <p>2023.9.8</p>
+            </div>
+          </button>
+          <button
+            className="reserveBtn"
+            onClick={navigate('/payment')}
+            onMouseMove={handleMouseMove}
+            style={{
+              background: `radial-gradient(circle at ${gradientPosition.x}px ${gradientPosition.y}px, #FF385C, #E61E4D, #E31C5F, #D70466, #BD1E59, #BD1E59)`,
+            }}
+          >
+            예약하기
+          </button>
+        </form>
+        <p className="subText">예약 확정 전에는 요금이 청구되지 않습니다.</p>
+        <div className="reservationInfoBox">
+          <div className="reservationInfo">
+            <p>￦36,000 x 5박</p>
+            <p>￦1,800,000</p>
+          </div>
+          <div className="reservationInfo">
+            <p>에어비앤비 서비스 수수료</p>
+            <p>￦279,531</p>
+          </div>
+        </div>
+      </div>
+      <div className="reservationFooter">
+        <p>총 합계</p>
+        <p>￦2,079,531</p>
+      </div>
+    </div>
+  );
 };
 
 export default ReservationBox;
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -44,98 +44,103 @@ const DetailPage = () => {
   }, []);
 
   return (
-    <div className="detailContainer">
-      <div className="detailHeader">
-        <div className="title">강원도 영월에 위치한 통나무집 A</div>
-        <div className="subTitle">
-          별점 후기 슈퍼호스트 Yeongwol-gun, 강원도, 한국
+    <div className="detail">
+      <div className="detailContainer">
+        <div className="detailHeader">
+          <div className="title">강원도 영월에 위치한 통나무집 A</div>
+          <div className="subTitle">
+            별점 후기 슈퍼호스트 Yeongwol-gun, 강원도, 한국
+          </div>
         </div>
-      </div>
-      <div className="detailImageBox">
-        {IMAGES.map(image => (
-          <img
-            className="detailImage"
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-          />
-        ))}
-      </div>
-      <div className="detailContentBox">
-        <div className="hostInfo">
-          <div className="hostInfoHeaderBox">
-            <div className="hostInfoHeader">
-              <h1 className="title">Geumju 님이 호스팅하는 통나무집 전체</h1>
-              <p className="subtitle">최대 인원 7명침실 2개침대 4개욕실 0개</p>
-            </div>
+        <div className="detailImageBox">
+          {IMAGES.map(image => (
             <img
-              src="https://a0.muscache.com/im/pictures/5874cae5-198f-4301-91af-d3eb137dd6c4.jpg?im_w=960"
-              alt="프로필"
+              className="detailImage"
+              key={image.id}
+              src={image.src}
+              alt={image.alt}
             />
-          </div>
-          <div className="itemInfo">
-            <div className="infoIcon">
-              <BiBed size={30} />
-              <p className="infoIconText">셀프 체크인</p>
-            </div>
-            <div className="infoIcon">
-              <BiBed size={30} />
-              <p className="infoIconText">유자님은 슈퍼호스트입니다</p>
-            </div>
-            <div className="infoIcon">
-              <BiBed size={30} />
-              <p className="infoIconText">
-                8월 26일 오후 12:00 전까지 무료로 취소하실 수 있습니다.
-              </p>
-            </div>
-          </div>
-          <div className="place">
-            <h1 className="title">숙박 장소</h1>
-            <div className="placeInfoContent">
-              <BiBed size={30} />
-              <p className="contentTitle">침실</p>
-              <p className="contentDetail">퀸사이즈 침대 1개</p>
-            </div>
-          </div>
-          <div className="convenient">
-            <h1 className="title">숙소 편의시설</h1>
-            <div className="convenientContent">
-              <div className="contentIcon">
-                <BiBed size={30} />
-                <p>강 전망</p>
+          ))}
+        </div>
+        <div className="detailContentBox">
+          <div className="hostInfo">
+            <div className="hostInfoHeaderBox">
+              <div className="hostInfoHeader">
+                <h1 className="title">Geumju 님이 호스팅하는 통나무집 전체</h1>
+                <p className="subtitle">
+                  최대 인원 7명침실 2개침대 4개욕실 0개
+                </p>
               </div>
-              <div className="contentIcon">
+              <img
+                src="https://a0.muscache.com/im/pictures/5874cae5-198f-4301-91af-d3eb137dd6c4.jpg?im_w=960"
+                alt="프로필"
+              />
+            </div>
+            <div className="itemInfo">
+              <div className="infoIcon">
                 <BiBed size={30} />
-                <p>전용 온수 욕조</p>
+                <p className="infoIconText">셀프 체크인</p>
               </div>
-              <div className="contentIcon">
+              <div className="infoIcon">
                 <BiBed size={30} />
-                <p>공용 수영장</p>
+                <p className="infoIconText">유자님은 슈퍼호스트입니다</p>
               </div>
-              <div className="contentIcon">
+              <div className="infoIcon">
                 <BiBed size={30} />
-                <p>140인치 HDTV</p>
+                <p className="infoIconText">
+                  8월 26일 오후 12:00 전까지 무료로 취소하실 수 있습니다.
+                </p>
               </div>
             </div>
-          </div>
-          <div className="calendarReservation">
-            <h1 className="title">Yeongwol-gun에서 6박</h1>
-            <Calendar />
-          </div>
-          <div className="review">
-            <div className="reviewHeader">
-              <AiFillStar size={22} />
-              <p>4.90</p>
-              <p>후기 240개</p>
+            <div className="place">
+              <h1 className="title">숙박 장소</h1>
+              <div className="placeInfoContent">
+                <BiBed size={30} />
+                <p className="contentTitle">침실</p>
+                <p className="contentDetail">퀸사이즈 침대 1개</p>
+              </div>
             </div>
-            <div className="reviewContent">
-              <ReviewItem />
-              <ReviewItem />
+            <div className="convenient">
+              <h1 className="title">숙소 편의시설</h1>
+              <div className="convenientContent">
+                <div className="contentIcon">
+                  <BiBed size={30} />
+                  <p>강 전망</p>
+                </div>
+                <div className="contentIcon">
+                  <BiBed size={30} />
+                  <p>전용 온수 욕조</p>
+                </div>
+                <div className="contentIcon">
+                  <BiBed size={30} />
+                  <p>공용 수영장</p>
+                </div>
+                <div className="contentIcon">
+                  <BiBed size={30} />
+                  <p>140인치 HDTV</p>
+                </div>
+              </div>
             </div>
+            <div className="calendarReservation">
+              <h1 className="title">Yeongwol-gun에서 6박</h1>
+              <Calendar />
+            </div>
+          </div>
+          <div className="paymentContainer">
+            <ReservationBox />
           </div>
         </div>
-        <div className="paymentContainer">
-          <ReservationBox />
+      </div>
+
+      <div className="review">
+        <div className="reviewHeader">
+          <AiFillStar size={22} />
+          <p>4.90</p>
+          <p>후기 240개</p>
+        </div>
+        <div className="reviewContent">
+          <ReviewItem />
+          <ReviewItem />
         </div>
       </div>
     </div>
