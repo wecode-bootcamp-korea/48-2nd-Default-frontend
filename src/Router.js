9ebcb6361d0146a560c95832e5179f149a8a97a7
llinjae <dldlswognqh@naver.com>
2023-08-27 14:00:27 +0900

    Remove : Router 컴포넌트에 CookieProvider 제거

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
 import DetailPage from './pages/DetailPage/DetailPage';
 import Navbar from './components/Navbar/Navbar';
 import Main from './pages/Main/Main';
@@ -8,16 +7,14 @@ import WireFrame from './components/Navbar/WireFrame';
 
 const Router = () => {
   return (
-    <CookiesProvider>
-      <BrowserRouter>
-        <Navbar />
-        <WireFrame />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/detail" element={<DetailPage />} />
-        </Routes>
-      </BrowserRouter>
-    </CookiesProvider>
+    <BrowserRouter>
+      <Navbar />
+      <WireFrame />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
