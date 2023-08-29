51b6a9a064bc1149f5c06579ea286f787c5b4577
llinjae <dldlswognqh@naver.com>
2023-08-29 16:09:06 +0900

    리뷰 작성 모달 구현 중

--- src/components/Detail/WriteReviewModal.jsx
+++ src/components/Detail/WriteReviewModal.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './WriteReviewModal.scss';
 
 const WriteReviewModal = () => {
-  return <div className="writeModalContainer">WriteReviewModal</div>;
+  return (
+    <div className="writeModalContainer">
+      <form className="writeModal">
+        <textarea />
+        <button>작성하기</button>
+      </form>
+    </div>
+  );
 };
 
 export default WriteReviewModal;
